0d87fd7580c7b4575cbd66477789f3f98f440ef7
Mai Ngoc Khang <khang.mn176791@sis.hust.edu.vn>
2021-07-04 12:05:43 +0700

    Filter BaseOn Distance

--- happy/main.go
+++ happy/main.go
@@ -12,6 +12,12 @@ var numberWorker int = 5
 var jobs = make(chan Job,numberWorker)
 var results = make(chan Job,numberWorker)
 var res = make([]Job,numberWorker)
+
+func RemoveIndex(s []int, index int) []int {
+	ret := make([]int, 0)
+	ret = append(ret, s[:index]...)
+	return append(ret, s[index+1:]...)
+}
 func findAndRemove(array []int,find int) []int{
 	if len((array)) == 0 || (array)[0] == find {
 		return []int{}
@@ -43,27 +49,35 @@ func worker(ignore_array []int,jobs<-chan Job,res []Job){
 
 
 }
-
+func filterBaseOnDis(realDis,refDis float32) bool{
+	if realDis > refDis{
+		return false
+	}
+	return true
+}
 func main() {
 	array := []int{1,2,3,4,9,6,5,7,8,10}
-	ignore_array := []int{2,6,9}
-	number :=len(array)
-
-	offer := int(number/numberWorker)
-	// sinh ra cac worker
-	for i:=1;i<=numberWorker;i++{
-		go worker(ignore_array,jobs,res)
-	}
-	// nap work vao channel jobs
-		for j:=0;j<numberWorker;j++{
-			jobs <- Job{array[j*offer:(j+1)*offer]}
-			if(j == (numberWorker-1)){
-				jobs <-Job{array[j*offer:]}
-			}
-		}
-		close(jobs)
-	wg.Wait()
-	fmt.Println(res)
+	array = RemoveIndex(array,5)
+	fmt.Println(array)
+	fmt.Println(filterBaseOnDis(21,20))
+	//ignore_array := []int{2,6,9}
+	//number :=len(array)
+	//
+	//offer := int(number/numberWorker)
+	//// sinh ra cac worker
+	//for i:=1;i<=numberWorker;i++{
+	//	go worker(ignore_array,jobs,res)
+	//}
+	//// nap work vao channel jobs
+	//	for j:=0;j<numberWorker;j++{
+	//		jobs <- Job{array[j*offer:(j+1)*offer]}
+	//		if(j == (numberWorker-1)){
+	//			jobs <-Job{array[j*offer:]}
+	//		}
+	//	}
+	//	close(jobs)
+	//wg.Wait()
+	//fmt.Println(res)
 
 
 }
